7af857caea8a8ceb3961a2c5829003d5a01086bb
thatchayut <thatchayut.u@gmail.com>
2019-01-05 22:54:02 +0700

    create pathway data in format of [pathway name, [[gene entrez id 1, gene expression 1], [gene entrez id 2, gene expression 2], ... ]]

--- pathway_expression.py
+++ pathway_expression.py
@@ -4,7 +4,7 @@ import pandas as pd
 def main():
     # prepare files to be used
     cols_to_read_file_to_convert = ["ID_REF", "GSM36777"]
-    rows_to_read_file_pathway = 1
+    rows_to_read_file_pathway = 1329
     file_ref = pd.read_csv("accession_number_to_entrez_id.csv")
     file_to_convert = pd.read_csv("GSE2034-22071 (edited).csv", usecols = cols_to_read_file_to_convert)
     file_pathway = pd.read_csv("c2.cp.v6.2.entrez.gmt.csv", nrows = rows_to_read_file_pathway)
@@ -41,7 +41,7 @@ def main():
         list_gene_expression = []
         for element in file_pathway.iloc[i, 2:-1]:
             if (str(element).isnumeric()):
-                print(element)
+                list_gene_name_with_expression = []
                 # check if genes in each pathway have their expression value
                 list_gene_same_entrez = []
                 for list_entrez_id_index in range(0, len(list_entrez_id)):
@@ -51,19 +51,26 @@ def main():
                 # if gene expression is not found, assume it as 'zero'
                 if not list_gene_same_entrez:
                     list_gene_same_entrez.append(0.0)
-                print(list_gene_same_entrez)
+                
                 # calculate genes expression of the same entrez id by using their average
                 num_of_same_entrez = len(list_gene_same_entrez)
                 avg_gene_expression = (sum(list_gene_same_entrez) / num_of_same_entrez)
                 avg_gene_expression = round(avg_gene_expression, 7)
-                # print(list_gene_same_entrez)
-                list_gene_expression.append(avg_gene_expression)
+                
+                # create a gene list in format [gene entrez id, gene expression]
+                list_gene_name_with_expression.append(element)
+                list_gene_name_with_expression.append(avg_gene_expression)
+                list_gene_expression.append(list_gene_name_with_expression)
+                # list_gene_expression.append(element)
+                # list_gene_expression.append(avg_gene_expression)
+
         # combine pathway name with its gene expressions
         pathway.append(pathway_name)
         pathway.append(list_gene_expression)
         
         pathways[key] = pathway
-    print(pathways[0])
+    print(pathways[1])
+    # print(len(pathways))
                     
 
 
