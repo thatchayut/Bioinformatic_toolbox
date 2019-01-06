328e4cc37963c07d24928a0d9f6e92fa9bef9c44
thatchayut <thatchayut.u@gmail.com>
2019-01-06 23:34:00 +0700

    separate input data into testing and training data

--- pac.py
+++ pac.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import random
+import math
+import calculate
 
 def main():
     # prepare data
@@ -29,6 +32,51 @@ def main():
     list_sample_no_relapse = []
     for element in sample_no_relapse.loc[:, 'GEO asscession number']:
         list_sample_no_relapse.append(element)
+        
+    # shuffle data to make each chunk does not depend on sample order
+    random.shuffle(list_sample_relapse)
+    print("list_sample_relapse SIZE = " + str(len(list_sample_relapse)))
+    random.shuffle(list_sample_no_relapse)
+    print("list_sample_no_relapse SIZE = " + str(len(list_sample_no_relapse)))
+
+    # get number of folds
+    while True:
+        num_of_folds = input("Number of folds: ")
+        if (num_of_folds.isnumeric() == False):
+            print("WARNING : Invalid input must be numeric")
+        elif(int(num_of_folds) > len(list_sample_relapse)):
+            print("WARNING : Number of folds exceeds the size of the 1st dataset")
+        elif(int(num_of_folds) > len(list_sample_no_relapse)):
+            print("WARNING : Number of folds exceeds the size of the 2nd dataset")
+        elif(int(num_of_folds) <= 1):
+            print("WARNING : Number of folds cannot lower than or equal to 1")
+        else:
+            break
+    num_of_folds = int(num_of_folds)
+
+    # split data into k parts
+    chunk_relapse_size = math.ceil(len(list_sample_relapse) / num_of_folds)
+    chunk_no_relapse_size = math.ceil(len(list_sample_no_relapse) / num_of_folds)
+
+    chunk_list_relapse = list(calculate.chunks(list_sample_relapse, chunk_relapse_size))
+    print("# chunks in chunk_list_relapse = " + str(len(chunk_list_relapse)))
+
+    chunk_list_no_relapse = list(calculate.chunks(list_sample_no_relapse, chunk_no_relapse_size))
+    print("# chunks in chunk_list_no_relapse  = " + str(len(chunk_list_no_relapse)))
+
+    check_valid, num_of_chunks = calculate.checkEqualListSize(chunk_list_relapse, chunk_list_no_relapse)
+
+    # do only if number of chunks of both datasets are equal
+    if (check_valid == True):
+        for chunk_test_index in range(0, num_of_chunks):
+            # separating data into testing and training dataset
+            chunk_test_relapse = chunk_list_relapse[chunk_test_index]
+            chunk_test_no_relapse = chunk_list_no_relapse[chunk_test_index]
+
+            print("\n------------------------------------------ K : " + str(chunk_test_index + 1) + " --------------------------------")
+            print("test relapse =" + str(chunk_test_relapse))
+            print("test no relapse = " + str(chunk_test_no_relapse))
+            
 
 if __name__ == '__main__':
     main()
