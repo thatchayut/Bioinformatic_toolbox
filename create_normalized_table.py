b5048353c5c469a230881d6089e5a9a1977f678a
thatchayut <thatchayut.u@gmail.com>
2019-01-18 21:51:56 +0700

    write pathway activity to file

--- create_normalized_table.py
+++ create_normalized_table.py
@@ -3,6 +3,9 @@ import pandas as pd
 import calculate
 
 def main():
+    # ask for output file name
+    file_name = input("Enter file name : ")
+
     # prepare data
     # row_to_read = 22283
     row_to_read = 22283
@@ -33,7 +36,8 @@ def main():
         pathway_name.append(i)
         pathway_name.append(file_pathway.loc[i, "PATHWAY_NAME"])
         list_pathway_name.append(pathway_name)
-    
+    # print(list_pathway_name)
+
     # consider non-relapse and relapse (not in specific period of time)
     sample_relapse = file_output.loc[file_output['relapse (1=True)'].isin(['1'])]
     sample_no_relapse = file_output.loc[file_output['relapse (1=True)'].isin(['0'])]
@@ -74,24 +78,87 @@ def main():
     print(" Max of all gene expression : " + str(max_all_gene_expression))
     print(" Min of all gene expression : " + str(min_all_gene_expression))
 
-# get gene expression of each pathway of each sample in testing set
-    samples_with_activity = {}
+    # get gene expression of each pathway of each sample in testing set
+    samples_with_gene_expression = {}
     for element_index in range(0, 3):
         print()
-        print("Creating pathways for samples_testing_all #" + str(element_index + 1) + "  is in progress ...")
+        print("Creating pathways for samples_with_gene_expression #" + str(element_index + 1) + "  is in progress ...")
         print(str(len(list_sample_all) - (element_index + 1)) + " samples left")
         print()
 
         sample = []
         sample_name = list_sample_all[element_index]
-        pathways = calculate.getPathway(file_ref_name, file_to_convert_name, file_pathway_name, sample_name, rows_to_read_file_pathway, max_of_data = max_all_gene_expression, min_of_data = min_all_gene_expression, \
-                    method = "wide_scaling")
+        pathways = calculate.getPathway(file_ref_name, file_to_convert_name, file_pathway_name, sample_name, rows_to_read_file_pathway, mean_of_data = mean_all_gene_expression, sd_of_data = sd_all_gene_expression, \
+                    method = "z_score")
 
         sample.append(sample_name)
         sample.append(pathways)
-        samples_with_activity[element_index] = sample 
+        samples_with_gene_expression[element_index] = sample 
 
+        # print(samples_with_gene_expression[0])
 
+    # calculate pathway activity using 'Mean'
+    samples_with_pathway_activity = {}
+    # { GSM1234, {0: ['KEGG_GLYCOLYSIS_GLUCONEOGENESIS', [[55902, 0.0], [2645, 0.0], ...}}
+    for samples_index in range(0, len(samples_with_gene_expression)):
+        sample = []
+        list_pathway = []
+        for pathway_index in range(0, len(samples_with_gene_expression[samples_index][1])):
+            list_gene_expression_in_pathway = []
+            pathway = []
+            for gene_index in range(0, len(samples_with_gene_expression[samples_index][1][pathway_index][1])):
+                # print(sample_relapse[samples_index][1][pathway_index][gene_index][1])
+                # print(gene_index)
+                gene_expression = samples_with_gene_expression[samples_index][1][pathway_index][1][gene_index][1]
+                list_gene_expression_in_pathway.append(gene_expression)
+
+            # data to collect as pathway activity
+            pathway_name = samples_with_gene_expression[samples_index][1][pathway_index][0]
+            pathway_activity = calculate.mean(list_gene_expression_in_pathway)
+
+            pathway.append(pathway_name)
+            pathway.append(pathway_activity)
+
+            list_pathway.append(pathway)
+            # # add to list only if it is in feature set
+            # if (pathway_name in list_top_ranked_pathways):
+            #     list_pathway.append(pathway)
+        
+        sample_name = samples_with_gene_expression[samples_index][0]
+        sample.append(sample_name)
+        sample.append(list_pathway)
+        samples_with_pathway_activity[samples_index] = sample    
+    
+    # print(samples_with_pathway_activity[0])
+
+    # write to file
+    #initiate worksheet
+    workbook = xlwt.Workbook()
+    worksheet = workbook.add_sheet(file_name)
+    style = xlwt.easyxf("align : horiz right")
+
+    print("Writing in progress ...")
+    print()
+    worksheet.write(0, 0, "PATHWAY_NAME")
+
+    # write pathway name
+    for i in range(0, len(list_pathway_name)):
+        worksheet.write(i + 1, 0, str(list_pathway_name[i][1]))
+
+    # write data of each sample
+    num_of_total_line_in_file = len(list_pathway_name) + 1
+    for sample_index in range(0, len(samples_with_pathway_activity)):
+        for line_index in range(0, num_of_total_line_in_file):
+            # write column name (sample id)
+            if (line_index == 0):
+                sample_name = str(samples_with_pathway_activity[sample_index][0])
+                worksheet.write(line_index, sample_index + 1, sample_name)
+            else:
+                pathway_activity = str(samples_with_pathway_activity[sample_index][1][line_index - 1][1])
+                worksheet.write(line_index, sample_index + 1, pathway_activity)
+    print(samples_with_pathway_activity[0])
+    workbook.save(file_name + ".csv")
+    print(" Creating file is done ...")
 
 if __name__ == '__main__':
     main()
